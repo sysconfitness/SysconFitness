5b4a42c1da7d7d4764189ead93a93d8b90ce7342
uilnew18@gmail.com <uilnew18@gmail.com>
2018-04-28 16:25:22 -0300

    hash cold end equols

--- SysconFitness-BackEnd/src/main/java/br/com/sysconFitness/model/Bairro.java
+++ SysconFitness-BackEnd/src/main/java/br/com/sysconFitness/model/Bairro.java
@@ -39,4 +39,30 @@ public class Bairro {
 	public String toString() {
 	    return String.format("%s[id=%d]", getClass().getSimpleName(), getId());
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bairro other = (Bairro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
